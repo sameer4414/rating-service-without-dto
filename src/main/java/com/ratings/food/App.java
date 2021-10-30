2a63c0b75278567e8b7f36b664840166d204dc5f
Shipra Srivastava <shipra3086@gmail.com>
2021-10-30 12:17:08 +0100

    My first commit

--- src/main/java/com/ratings/food/App.java
+++ src/main/java/com/ratings/food/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main( String[] args ) {
     	SpringApplication.run(App.class, args);
-        System.out.println( "Hello World from ratings client!" );
+        System.out.println( "Hello World 123 from ratings client!" );
     }
     
 }
